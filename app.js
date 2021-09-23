21c81972e7ea4830cf26e963ace3719fa7987458
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-09-23 11:44:22 +0530

    added custom X-header and teapot respose

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-	res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
+app.get("/demo", (req, res) => {
+  res.set("X-full-stack", "4life");
+  res.status(418).send("I prefer coffee");
+});
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening on port ${port}!`);
 });
